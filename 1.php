30d4d79bd990df42d7bd61381b58af04d8d4f359
pandaliveTV <50134635+pandaliveTV@users.noreply.github.com>
2020-09-13 23:32:37 +0200

    Add files via upload

---
+++ 1.php
@@ -0,0 +1,10 @@
+<?php
+error_reporting(0);
+ini_set('user_agent', 'Android Vinebre Software');
+$homepage = file_get_contents('https://app.liveplanettv.com/beta/api.php?device_id=');
+$pR = '/(?<=wmsAuthSign": ")(.*?)(?=")/';
+ preg_match_all($pR, $homepage, $matches);
+$page= implode(', ',$matches[1]);
+
+echo "http://163.172.223.102:8081/arab@arb/beinsports4/playlist.m3u8?wmsAuthSign=".$page;
+?>
